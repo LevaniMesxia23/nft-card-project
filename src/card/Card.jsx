996b6a4061595c846f07e2f4d12e7aa936a77774
levan mesxia <levanmesxia@levans-MacBook-Pro.local>
2024-03-11 23:22:13 +0400

    add project

---
+++ src/card/Card.jsx
@@ -0,0 +1,30 @@
+import "./card.css"
+import Equilibrium from "/images/image-equilibrium.jpg"
+import Ethereum from "/images/icon-ethereum.svg"
+import Clock from "/images/icon-clock.svg"
+import Avatar from "/images/image-avatar.png"
+export default function Card(){
+  return (
+    <div className="container">
+      <img src={Equilibrium} alt="" />
+      <h1 className="title">Equilibrium #3429</h1>
+      <p className="text">Our Equilibrium collection promotes balance and calm.</p>
+      <div className="info-container">
+        <div className="eth">
+          <img src={Ethereum} alt="" />
+          <span>0.041 ETH</span>
+        </div>
+        <div className="clock">
+          <img src={Clock} alt="" />
+          <span>3 days left</span>
+        </div>
+      </div>
+      <div className="bottom">
+        <img src={Avatar} alt="" />
+        <p>
+        Creation of<span>Jules Wyvern</span>
+        </p>
+      </div>
+    </div>
+  )
+}
